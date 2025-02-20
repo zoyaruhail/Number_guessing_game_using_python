430fdfa5b02320848d1e710856ebd1a14cb7e79a
Zoya Ruhail <109350804+zoyaruhail@users.noreply.github.com>
2025-02-20 22:47:42 +0530

    Add files via upload

---
+++ number_guessing.py
@@ -0,0 +1,33 @@
+import random
+def guessing():
+    print("Welcome to the Number Guessing Game !!")
+    score = 0
+    comp_score = 0
+    while True:
+        try:
+            menu = int(input("Menu \n1. Start \n2. exit\nChoose from the Menu: "))
+            if menu == 1:
+                while True:
+                    num = random.randint(0,11)
+                    num1 = int(input("Guess the number from 0 - 10: "))
+                    if num == num1:
+                        score += 1
+                        comp_score += 0
+                        print(f"Wohooooo! Correct Guess you got a score!\nYour Score = {score} || Computer Score = {comp_score}")
+                    else:
+                        score += 0
+                        comp_score += 1
+                        print(f"Oppsie! Better luck next time :( Computer gets the score!\nYour Score = {score} || Computer Score = {comp_score}")
+                    again = input("Do you want to play again?? (y/n): ").lower()
+                    if again != 'y':
+                        break
+            elif menu == 2:
+                print(f"Thank You For Playing the Game !\nFinal Scores \nYour Score = {score} || Computer Score = {comp_score}")
+                break
+            else:
+                print("Invalid Input")
+        except ValueError:
+            print("Enter the required output")
+
+
+guessing()
